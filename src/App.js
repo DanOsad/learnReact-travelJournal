cadb4aaebfcb4f466beb4b32ca7f5f092dc87103
DanOsad <dan@osadtsuk.com>
2022-07-27 15:36:58 +0300

    initial setup using CRA and cleaning of unused files as well as folder structuring and creation of necessary components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Header from './components/Header'
+import Location from './components/Location'
+import data from './data'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  const locations = data.map(location=>{
+    // do something with data
+  })
+  render (
+    <Header />
+    {locations}
+  )
 }
-
-export default App;
